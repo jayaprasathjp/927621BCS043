3bdb0e753b6d398883516e54a04cf94544adda71
jayaprasathjp <jayaprasathjp44@gmail.com>
2024-05-30 13:20:47 +0530

    final

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,23 @@
 import React, { useEffect, useState } from "react";
 
-import { useDebouncedCallback } from "use-debounce";
 function App() {
   const [data, setData] = useState([]);
-  const [company, setCompany] = useState('');
   const [product, setProduct] = useState('');
   const [top, SetTop] = useState('');
   const [minPrice, SetMinPrice] = useState('');
   const [MaxPrice, setMaxPrice] = useState('');
   
 
-  // const handleSearch = useDebouncedCallback(async (term) => {
-  //   try {
-  //     const newData = data.filter((user) => {
-  //       return (
-  //         user.firstName.toLowerCase().includes(term.toLowerCase()) ||
-  //         user.lastName.toLowerCase().includes(term.toLowerCase()) ||
-  //         user.email.toLowerCase().includes(term.toLowerCase()) ||
-  //         user.id.toString().includes(term) ||
-  //         user.contactNumber.includes(term)
-  //       );
-  //     });
-  //     setFiltered(newData);
-  //     setPageNo([Math.ceil(newData.length/show),pageNo[1]]);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }, 300);
-  // useEffect(() => {
-  //   const handleFetch = async () => {
-  //     const response = await fetch(
-  //       `http://localhost:4000/categories/Mouse/products`
-  //     );
-  //     const data = await response.json();
-  //     setData(data);
-  //     // setFiltered(data);
-  //     // setPageNo([data.length/5,1]);
-  //   };
-  //   handleFetch();
-  // }, []);
+  useEffect(() => {
+    const handleFetch = async () => {
+      const response = await fetch(
+        `http://localhost:4000/categories/${product}/products?top=${top}&minPrice=${minPrice}&maxPrice=${MaxPrice}`
+      );
+      const data = await response.json();
+      setData(data);
+    };
+    handleFetch();
+  }, []);
 
   return (
     <div>
@@ -51,22 +30,7 @@ function App() {
         Products
       </h2>
       <div className="flex justify-center items-center space-x-2 ">
-      <select
-          id="company"
-          onChange={(e) => {
-            setCompany(e.target.value)
-          }}
-          className="bg-gray-50 border h-10 w-36 border-gray-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 outline-none focus:border-indigo-500 block p-2.5 "
-        >
-          <option>company</option>
-          <option value="AMZ" select="true">
-            AMZ
-          </option>
-          <option value="FLP">FLP</option>
-          <option value="SNP">SNP</option>
-          <option value="MYN">MYN</option>
-          <option value="AZO">AZO</option>
-        </select>
+     
         <input name="product"  onChange={(e)=>{setProduct(e.target.value)}} className="bg-gray-50 border h-10 w-36 border-gray-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 outline-none focus:border-indigo-500 block p-2.5 " placeholder="Product"/>
         <input name="top" onChange={(e)=>{SetTop(e.target.value)}} className="bg-gray-50 border h-10 w-36 border-gray-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 outline-none focus:border-indigo-500 block p-2.5 " placeholder="TOP"/>
         <input name="minPrice" onChange={(e)=>{SetMinPrice(e.target.value)}} className="bg-gray-50 border h-10 w-36 border-gray-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 outline-none focus:border-indigo-500 block p-2.5 " placeholder="Min Price"/>
@@ -77,6 +41,9 @@ function App() {
         <table className="text-sm text-left rtl:text-right text-gray-500  border-collapse border">
           <thead className="text-xs text-indigo-700 uppercase bg-indigo-50 rounded-md">
             <tr>
+              <th scope="col" className="px-6 py-3">
+                ID
+              </th>
               <th scope="col" className="px-6 py-3">
                 Product
               </th>
@@ -104,8 +71,14 @@ function App() {
                     scope="row"
                     className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
                   >
-                    {product.ProductName}
+                    {product.id}
                   </th>
+                  <td
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                  >
+                    {product.ProductName}
+                  </td>
                   <td className="px-6 py-4">
                     {product.company}
                   </td>
--- server/index.js
+++ server/index.js
@@ -2,60 +2,51 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
-const helmet = require("helmet");
-const morgan = require("morgan");
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
 const app = express();
-app.use(
-  helmet({
-    crossOriginResourcePolicy: false,
-  })
-);
 app.use(cors());
 app.use(bodyParser.json());
-app.disable("x-powered-by");
-app.use(morgan("dev"));
-
-// const handleFetch=async ()=>{
-//   const response= await axios.get(`http://20.244.56.144/test/companies/AMZ/categories/phone/products?top=5&minPrice=100&maxPrice=1000`, {
-//     headers: {
-//       'Authorization': `Bearer ${process.env.BEARER_TOKEN}`
-//     }
-//   });;
-//   console.log(response.data);
-// }
-// handleFetch();
-
-// app.get('/categories/:categoryname/products',(req, res)=>{
-//   const categoryName=req.params.categoryname;
-//   console.log(categoryName);
-
-//   res.json(categoryName);
-
-// })
 let accessToken ;
 async function refreshAccessToken(){
-  accessToken = await fetch("http://20.244.56.144/test/auth",{
+  let res = await fetch("http://20.244.56.144/test/auth",{
     method:"POST",
-    
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body:JSON.stringify({
+      "companyName": "goMart",
+      "clientID": "e9e2b380-36ad-4b2f-810d-eb26239d9210",
+      "clientSecret": "kDBtXfUZrFNjRgkq",
+      "ownerName": "Jayaprasath",
+      "ownerEmail": "927621bcs043@mkce.ac.in",
+      "rollNo": "927621BCS043"
+  })
   });
+
+  let data =  await res.json();
+
+
+  accessToken = data.access_token;
 }
 const fetchProducts = async (company, category, top,minPrice, maxPrice) => {
 
+  console.log(accessToken);
   try {
+      if(!accessToken){
+        throw new Error("access token not found");
+      }
       const response = await axios.get(`http://20.244.56.144/test/companies/${company}/categories/${category}/products?top=${top}&minPrice=${minPrice}&maxPrice=${maxPrice}`, {headers: {
                   'Authorization': `Bearer ${accessToken}`
                 }
       });
       return response.data.map(product => ({
           ...product,
-          uniqueId: uuidv4(),
+          id: uuidv4(),
           company,
       }));
-      console.log(response.data);
   } catch (error) {
-      console.error(`Error ${company}:`);
+      console.error(`Error ${company}:` );
       return [];
   }
 };
@@ -65,6 +56,7 @@ app.get('/categories/:categoryname/products', async (req, res) => {
   const { top,minPrice, maxPrice} = req.query;
   const companies = ['AMZ', 'BLA', 'CBY', 'DOM', 'EVG']; 
 
+  await refreshAccessToken()
 
   try {
       let allProducts = [];
@@ -79,33 +71,6 @@ app.get('/categories/:categoryname/products', async (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
-// app.get('/categories/:categoryname/products/:productid', async (req, res) => {
-//   const { categoryname, productid } = req.params;
-
-//   try {
-//       let productDetails = null;
-      
-//       // Fetch all products in the category to find the product by uniqueId
-//       const companies = ['AMZ', 'BLA', 'CBY', 'DOM', 'EVG'];
-//       for (const company of companies) {
-//           const products = await fetchProducts(company, categoryname);
-//           productDetails = products.find(product => product.uniqueId === productid);
-//           if (productDetails) {
-//               break;
-//           }
-//       }
-
-//       if (productDetails) {
-//           res.json(productDetails);
-//       } else {
-//           res.status(404).json({ error: 'Product not found' });
-//       }
-//   } catch (error) {
-//       console.error('Error fetching product details:', error);
-//       res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on http://localhost:${process.env.PORT}`);
